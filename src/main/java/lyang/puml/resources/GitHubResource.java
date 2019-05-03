1a78a5e757847321f370fd004318131410182742
lyang <github@linyang.me>
2019-05-03 00:14:08 -0700

    Add no-cache/expires-at header to ensure diagrams are always up to date

--- src/main/java/lyang/puml/resources/GitHubResource.java
+++ src/main/java/lyang/puml/resources/GitHubResource.java
@@ -2,6 +2,7 @@ package lyang.puml.resources;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Date;
 import java.util.Optional;
 
 import net.sourceforge.plantuml.SourceStringReader;
@@ -11,15 +12,19 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
+
 import lyang.puml.configurations.GitHubConfig;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
 @Path("/github")
 public class GitHubResource {
+
   private static final String GITHUB_RAW_FORMAT = "application/vnd.github.v3.raw";
+  private static final String URL_FORMAT = "https://%s/repos/%s/%s/contents/%s";
   private OkHttpClient client;
   private GitHubConfig config;
 
@@ -33,20 +38,26 @@ public class GitHubResource {
   @Produces("image/png")
   @Path("/{target}/{user}/{repo}/{path:.+}")
   public Response get(@PathParam("target") String target,
-                      @PathParam("user") String user,
-                      @PathParam("repo") String repo,
-                      @PathParam("path") String path) throws IOException {
-    try (okhttp3.Response response = client.newCall(buildRequest(target, user, repo, path)).execute()) {
-      return Response.ok(render(response.body().string())).build();
+      @PathParam("user") String user,
+      @PathParam("repo") String repo,
+      @PathParam("path") String path) throws IOException {
+    Request request = buildRequest(target, user, repo, path);
+    try (okhttp3.Response response = client.newCall(request).execute()) {
+      return Response.ok(render(response.body().string()))
+          .cacheControl(CacheControl.valueOf("no-cache"))
+          .expires(new Date())
+          .build();
     }
   }
 
   private Request buildRequest(String target, String user, String repo, String path) {
     GitHubConfig.Config config = this.config.targets.get(target);
-    String url = String.format("https://%s/repos/%s/%s/contents/%s", config.endpoint, user, repo, path);
-    Request.Builder builder = new Request.Builder().addHeader(HttpHeaders.ACCEPT, GITHUB_RAW_FORMAT).url(url).get();
+    String url = String.format(URL_FORMAT, config.endpoint, user, repo, path);
+    Request.Builder builder = new Request.Builder()
+        .addHeader(HttpHeaders.ACCEPT, GITHUB_RAW_FORMAT)
+        .url(url).get();
     Optional.ofNullable(config.apiToken)
-      .ifPresent(token -> builder.addHeader(HttpHeaders.AUTHORIZATION, "token " + token));
+        .ifPresent(token -> builder.addHeader(HttpHeaders.AUTHORIZATION, "token " + token));
     return builder.build();
   }
 
