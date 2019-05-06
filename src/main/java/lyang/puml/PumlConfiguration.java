b8e2360d031887418c8accacc88e1bd8583f1d3e
lyang <github@linyang.me>
2019-05-06 04:47:48 +0000

    Add credential support to config file; Make use of authentication helper

--- src/main/java/lyang/puml/PumlConfiguration.java
+++ src/main/java/lyang/puml/PumlConfiguration.java
@@ -1,7 +1,10 @@
 package lyang.puml;
 
 import io.dropwizard.Configuration;
+import java.util.List;
+import lyang.puml.configurations.Credential;
 
 public class PumlConfiguration extends Configuration {
 
+  public List<Credential> credentials;
 }
--- src/main/java/lyang/puml/PumlModule.java
+++ src/main/java/lyang/puml/PumlModule.java
@@ -3,12 +3,20 @@ package lyang.puml;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
+import lyang.puml.utils.AuthenticationHelper;
 import okhttp3.OkHttpClient;
 
 public class PumlModule extends AbstractModule {
+
   @Provides
   @Singleton
-  OkHttpClient httpClient() {
-    return new OkHttpClient();
+  OkHttpClient httpClient(AuthenticationHelper authenticationHelper) {
+    return new OkHttpClient.Builder().cache(null).addInterceptor(authenticationHelper).build();
+  }
+
+  @Provides
+  @Singleton
+  AuthenticationHelper authenticationHelper(PumlConfiguration configuration) {
+    return new AuthenticationHelper(configuration.credentials);
   }
 }
