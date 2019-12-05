b7088bd506a62c8f99d22b602c27e40e0201f747
Lin Yang <github@linyang.me>
2019-12-05 00:52:34 -0800

    Update app.sh

--- app.sh
+++ app.sh
@@ -4,4 +4,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-PORT=${PORT:-8080} JAVABIN=/usr/bin/java JAVA_RUNFILES=/app ./puml --singlejar --jvm_flags="${JVM_ARGS:-}" server config.yaml
+PORT=${PORT:-8080}
+JVM_FLAGS=${JVM_FLAGS:-}
+JAVABIN=$(which java)
+
+PORT=$PORT JAVABIN=$JAVABIN JAVA_RUNFILES=/app ./puml --singlejar --jvm_flags="$JVM_FLAGS" server config.yaml
