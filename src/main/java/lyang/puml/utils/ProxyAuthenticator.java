0d5e087d93f173f188ff3afe7912342108ad35e6
lyang <github@linyang.me>
2019-12-06 04:51:53 +0000

    Fix proxy host/port check

--- src/main/java/lyang/puml/utils/ProxyAuthenticator.java
+++ src/main/java/lyang/puml/utils/ProxyAuthenticator.java
@@ -1,11 +1,12 @@
 package lyang.puml.utils;
 
+import java.util.Objects;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import okhttp3.Authenticator;
 import okhttp3.Credentials;
-import okhttp3.HttpUrl;
 import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.Route;
@@ -16,31 +17,17 @@ public class ProxyAuthenticator implements Authenticator {
 
   @Override
   public Request authenticate(Route route, Response response) {
-    if (isConfiguredFor(response.request())) {
-      return setProxyAuth(response.request());
+    if (Objects.isNull(response.request().header(PROXY_AUTHORIZATION))) {
+      return setProxyAuth(route, response.request());
     }
     return null;
   }
 
-  private boolean isConfiguredFor(Request request) {
-    HttpUrl url = request.url();
-    return url.host().equalsIgnoreCase(proxyHost(url.scheme())) && url.port() == proxyPort(url.scheme());
-  }
-
-  private Request setProxyAuth(Request request) {
+  private Request setProxyAuth(Route route, Request request) {
     String scheme = request.url().scheme();
     String credential = Credentials.basic(proxyUser(scheme), proxyPassword(scheme));
-    LOGGER.info("Authenticating with {}:{}", request.url().host(), request.url().port());
-    LOGGER.info("Proxy User: {}", System.getProperty("https.proxyUser"));
+    LOGGER.info("Authenticating {}@{}", proxyUser(scheme), route.proxy().address());
     return request.newBuilder().addHeader(PROXY_AUTHORIZATION, credential).build();
-  }
-
-  private String proxyHost(String scheme) {
-    return System.getProperty(scheme + ".proxyHost");
-  }
-
-  private int proxyPort(String scheme) {
-    return Integer.parseInt(System.getProperty(scheme + ".proxyPort"));
   }
 
   private String proxyUser(String scheme) {
