9329bce807ea82c6501315a0c7ad8dee5efb10d4
Lin Yang <github@linyang.me>
2021-06-25 23:02:13 -0700

    Add support for private GHEs

--- src/main/java/lyang/puml/PumlConfiguration.java
+++ src/main/java/lyang/puml/PumlConfiguration.java
@@ -1,12 +1,15 @@
 package lyang.puml;
 
 import java.util.List;
+import java.util.Map;
 
 import com.google.common.collect.ImmutableList;
 
+import com.google.common.collect.ImmutableMap;
 import io.dropwizard.Configuration;
 import lyang.puml.configurations.Credential;
 
 public class PumlConfiguration extends Configuration {
   public List<Credential> credentials = ImmutableList.of();
+  public Map<String, String> hostMappings = ImmutableMap.of();
 }
--- src/main/java/lyang/puml/resources/GitHubResource.java
+++ src/main/java/lyang/puml/resources/GitHubResource.java
@@ -1,6 +1,7 @@
 package lyang.puml.resources;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lyang.puml.PumlConfiguration;
 import lyang.puml.utils.Puml;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -11,18 +12,22 @@ import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.util.Base64;
+import java.util.Map;
 
 @Path("/github")
 public class GitHubResource {
+  public static final String GH_API = "https://api.%s/repos/%s/%s/contents/%s?ref=%s";
   public static final String GH_JSON = "application/vnd.github.v3+json";
-  public static final String GH_API = "https://api.github.com/repos/%s/%s/contents/%s?ref=%s";
+  public static final String GITHUB = "github.com";
   private final OkHttpClient client;
   private final ObjectMapper mapper;
+  private final Map<String, String> hostMappings;
 
   @Inject
-  public GitHubResource(OkHttpClient client, ObjectMapper mapper) {
+  public GitHubResource(OkHttpClient client, ObjectMapper mapper, PumlConfiguration configuration) {
     this.client = client;
     this.mapper = mapper;
+    this.hostMappings = configuration.hostMappings;
   }
 
   @GET
@@ -35,9 +40,30 @@ public class GitHubResource {
       @PathParam("path") String path,
       @QueryParam("pumlIndex") @DefaultValue("0") int pumlIndex)
       throws IOException {
-    Request request = getRequest(owner, repo, commit, path);
+    Request request = getRequest(GITHUB, owner, repo, commit, path);
     try (okhttp3.Response response = client.newCall(request).execute()) {
       return Puml.renderToResponse(getContent(response), pumlIndex).build();
+    }
+  }
+
+  @GET
+  @Produces("image/png")
+  @Path("{host}/{owner}/{repo}/blob/{commit}/{path:.+}")
+  public Response get(
+          @PathParam("host") String host,
+          @PathParam("owner") String owner,
+          @PathParam("repo") String repo,
+          @PathParam("commit") String commit,
+          @PathParam("path") String path,
+          @QueryParam("pumlIndex") @DefaultValue("0") int pumlIndex)
+          throws IOException {
+    if (hostMappings.containsKey(host)) {
+      Request request = getRequest(hostMappings.get(host), owner, repo, commit, path);
+      try (okhttp3.Response response = client.newCall(request).execute()) {
+        return Puml.renderToResponse(getContent(response), pumlIndex).build();
+      }
+    } else {
+      return Response.status(Response.Status.NOT_FOUND).build();
     }
   }
 
@@ -46,8 +72,8 @@ public class GitHubResource {
     return new String(Base64.getDecoder().decode(encoded.replaceAll("\n", "")));
   }
 
-  private Request getRequest(String owner, String repo, String commit, String path) {
-    String url = String.format(GH_API, owner, repo, path, commit);
+  private Request getRequest(String host, String owner, String repo, String commit, String path) {
+    String url = String.format(GH_API, host, owner, repo, path, commit);
     return new Request.Builder().addHeader(HttpHeaders.ACCEPT, GH_JSON).url(url).build();
   }
 }
